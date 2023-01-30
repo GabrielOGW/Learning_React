fdab6998e6f5c6fe51813ee03d6a626cc1eef8aa
GabrielOGW <gabrielogassawara2001@gmail.com>
2023-01-30 11:19:19 -0300

    Use Effect

--- react_hooks/src/App.js
+++ react_hooks/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,12 +11,33 @@ function App() {
     setCount((prevCount) => prevCount + 1);
   }
 
+  const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourceType]);
+
   return (
-    <div className="App">
-      <button onClick={decrementar}>-</button>
-      <span>{count}</span>
-      <button onClick={incrementar}>+</button>
-    </div>
+    <>
+      <div className="UseState">
+        <button onClick={decrementar}>-</button>
+        <span>{count}</span>
+        <button onClick={incrementar}>+</button>
+      </div>
+      <br />
+      <div className="UseEffect">
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {items.map(item => {
+        return <pre>{JSON.stringify(item)}</pre>
+      })}
+    </>
   );
 }
 
